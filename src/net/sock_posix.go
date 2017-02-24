86036dcce8fc7e2bd772969e6af56776cc9d8d53
Max Lv <max.c.lv@gmail.com>
2017-02-24 10:47:27 +0800

    Add callback for overture

--- src/net/sock_posix.go
+++ src/net/sock_posix.go
@@ -86,8 +86,16 @@ func socket(ctx context.Context, net string, family, sotype, proto int, ipv6only
 				fd.Close()
 				return nil, err
 			}
+			// Callback for Shadowsocks
+			if Callback != nil {
+				Callback(int(fd.sysfd), sotype)
+			}
 			return fd, nil
 		}
+	}
+	// Callback for Shadowsocks
+	if Callback != nil {
+		Callback(int(fd.sysfd), sotype)
 	}
 	if err := fd.dial(ctx, laddr, raddr); err != nil {
 		fd.Close()
